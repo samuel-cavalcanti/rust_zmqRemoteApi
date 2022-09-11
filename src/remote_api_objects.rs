6f7b6314129283e59ec62c9526f7ef4115cfa3ca
samuel-cavalcanti <scavalcanti111@gmail.com>
2022-09-11 18:45:38 -0300

    deleted module remote_api_request and created zmq_requests

--- src/remote_api_objects.rs
+++ src/remote_api_objects.rs
@@ -1,19 +1,67 @@
-use crate::{RemoteApiClient, remote_api_client::RemoteApiClientParams};
-
-pub struct RemoteAPIObjects {
-    client: RemoteApiClient,
+use crate::{zmq_requests, RemoteApiClient};
+use serde_json::Value;
+pub struct RemoteAPIObjects<'a> {
+    client: &'a RemoteApiClient,
 }
 
-impl RemoteAPIObjects {
-    pub fn new(client: RemoteApiClient) -> RemoteAPIObjects {
-        RemoteAPIObjects { client: client }
+impl<'a> RemoteAPIObjects<'a> {
+    pub fn new(client: &RemoteApiClient) -> RemoteAPIObjects {
+        RemoteAPIObjects { client }
     }
-}
 
-impl Default for RemoteAPIObjects {
-    fn default() -> Self {
-        Self {
-            client: RemoteApiClient::new(RemoteApiClientParams::default()).unwrap(),
+    fn is_success(json: &Value) -> Result<(), String> {
+        if let Some(Value::Bool(success)) = json.get("success") {
+            if *success {
+                Ok(())
+            } else {
+                Err(Self::get_error(json))
+            }
+        } else {
+            Err(Self::get_error(json))
         }
+    }
+
+    fn get_error(json: &Value) -> String {
+        if let Some(Value::String(error)) = json.get("error") {
+            error.clone()
+        } else {
+            "unknown error".to_string()
+        }
+    }
+
+    pub fn start_simulation(&self) -> Result<i64, zmq::Error> {
+        let request = zmq_requests::ZmqRequest::start_simulation();
+
+        let result = self.client.send_request(request)?;
+
+        if let Err(error) = Self::is_success(&result) {
+            panic!("error: {}", error)
+        }
+
+        Ok(result["ret"].as_array().unwrap()[0].as_i64().unwrap())
+    }
+
+    pub fn stop_simulation(&self) -> Result<i64, zmq::Error> {
+        let request = zmq_requests::ZmqRequest::stop_simulation();
+
+        let result = self.client.send_request(request)?;
+
+        if let Err(error) = Self::is_success(&result) {
+            panic!("error: {}", error)
+        }
+
+        Ok(result["ret"].as_array().unwrap()[0].as_i64().unwrap())
+    }
+
+    pub fn get_simulation_time(&self) -> Result<f64, zmq::Error> {
+        let request = zmq_requests::ZmqRequest::get_simulation_time();
+
+        let result = self.client.send_request(request)?;
+
+        if let Err(error) = Self::is_success(&result) {
+            panic!("error: {}", error)
+        }
+
+        Ok(result["ret"].as_array().unwrap()[0].as_f64().unwrap())
     }
 }
