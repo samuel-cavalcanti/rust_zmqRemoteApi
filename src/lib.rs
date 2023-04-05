bb46b35bd1ec3ae3bb0c6f64bdd1a3ac95a06653
samuel-cavalcanti <scavalcanti111@gmail.com>
2023-04-05 09:08:59 -0300

    exposed low level api and SimIK

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,10 @@ mod remote_api_client;
 mod remote_api_objects;
 mod zmq_requests;
 
-pub use remote_api_client::{RemoteApiClient, RemoteApiClientParams};
-
+pub use remote_api_client::{RemoteApiClient, RemoteApiClientInterface, RemoteApiClientParams};
 pub use remote_api_objects::sim;
+pub use remote_api_objects::sim_ik;
+pub use zmq_requests::{RawRequest, ZmqRequest};
 
 pub use remote_api_objects::connection_error::RemoteAPIError;
 
